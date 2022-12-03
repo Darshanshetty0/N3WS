aed452196b254dc0307ea2646705b086c62fdeb9
biratdatta <birat.datta26@gmail.com>
2022-12-03 09:03:15 +0530

    changes

--- pages/news.js
+++ pages/news.js
@@ -1,11 +1,11 @@
-import { Footer, Navbar, CreatePost } from '../components';
-import { About, Explore, Feedback, GetStarted, Hero, Insights, WhatsNew, World, NewsCard } from '../sections';
+import { Navbar, CreatePost } from '../components';
+import { NewsCard } from '../sections';
 
 const News = () => (
   <div className="bg-primary-black overflow-hidden">
     <Navbar />
-    <NewsCard/>
-    <CreatePost/>
+    <NewsCard />
+    <CreatePost />
   </div>
 );
 
